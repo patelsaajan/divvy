be475dc37c03d818a178c199fd958e115e5f4aa0
Saajan Patel <saajan.patel@hoppstudio.com>
2025-06-21 10:32:05 +0100

    feat: initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  // css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',    
+  ],
+})
