6f67a970be068202de67c4b4a8d2877fca5f9b28
Saajan Patel <saajan.patel@hoppstudio.com>
2025-06-21 18:10:07 +0100

    feat: waiting for status to load template on receipt page

--- app/pages/receipt/[id]/index.vue
+++ app/pages/receipt/[id]/index.vue
@@ -1,4 +1,6 @@
 <template>
+
+  <template v-if="receiptStatus && itemsStatus === 'success'">
   <div class="container mx-auto mt-6">
     <div class="flex items-center mb-6">
       <UButton variant="ghost" class="p-2 mr-2" @click="navigateTo('/')">
@@ -10,7 +12,7 @@
     <div class="bg-gray-800 rounded-lg p-4 mb-4">
       <div class="flex items-center justify-between">
         <div class="flex items-center">
-          <h2>{{ expense.title }}</h2>
+          <h2>{{ receipt.title ?? 'Unknown' }}</h2>
         </div>
       </div>
     </div>
@@ -19,48 +21,51 @@
     <div class="mb-6">
       <h3 class="font-medium mb-3">Receipt</h3>
       <div class="bg-gray-800 rounded-lg p-4 mb-4">
+        <!-- Receipt Header -->
+        <div class="flex justify-between items-center mb-3 pb-3 border-b border-gray-700">
+          <div class="flex items-center">
+            <div>
+              <h4 class="font-medium">{{ receipt.vendor ?? 'Unknown' }}</h4>
+              <span class="text-xs text-gray-400">{{ formatDate(receipt.uploaded_at) ?? 'Unknown' }}</span>
+            </div>
+          </div>
+          <div class="text-right">
+            <span class="font-semibold text-lg">${{ receipt.total_cost.toFixed(2) }}</span>
+            <div class="text-xs text-gray-400">{{ receiptItems.length }} items</div>
+          </div>
+        </div>
 
-    <!-- Receipt Header -->
-    <div class="flex justify-between items-center mb-3 pb-3 border-b border-gray-700">
-      <div class="flex items-center">
-        <div>
-          <h4 class="font-medium">{{ expense.title }}</h4>
-          <span class="text-xs text-gray-400">{{ expense.date }}</span>
+        <!-- Receipt Line Items -->
+        <div class="space-y-2">
+          <div 
+            v-for="item in receiptItems" 
+            :key="item.id" 
+            class="flex justify-between items-center py-2"
+          >
+            <div class="font-medium">{{ item.title }}</div> 
+          <div class="font-medium">${{ item.cost ? item.cost.toFixed(2) : 0 }}</div>
+          </div>
+        </div>
+
+        <!-- Receipt Actions -->
+        <div class="flex space-x-2 mt-3 pt-3 border-t border-gray-700">
+          <NuxtLink to="/receipt/1/people" class="flex-1 bg-gray-700 text-white py-2 rounded text-sm flex items-center justify-center">
+            <UIcon name="i-heroicons-user-group" :size="16" class="inline mr-1" />
+            <span class="ml-2">People</span>
+          </NuxtLink>
+          <NuxtLink to="/receipt/1/edit" class="flex-1 bg-orange-500 text-white py-2 rounded text-sm flex items-center justify-center">
+            <UIcon name="i-heroicons-pencil" :size="16" class="inline mr-1" />
+            <span class="ml-2">Edit</span>
+          </NuxtLink>
         </div>
       </div>
-      <div class="text-right">
-        <span class="font-semibold text-lg">${{ expense.total.toFixed(2) }}</span>
-        <!-- <div class="text-xs text-gray-400">{{ receiptItems.data.value.length }} items</div> -->
-      </div>
-    </div>
-
-    <!-- Receipt Line Items -->
-    <div v-show="status === 'success'" class="space-y-2">
-      <div 
-        v-for="item in receiptItems" 
-        :key="item.id" 
-        class="flex justify-between items-center py-2"
-      >
-        <div class="font-medium">{{ item.title }}</div> 
-        <div class="font-medium">${{ item.cost.toFixed(2) }}</div>
-      </div>
-    </div>
-
-    <!-- Receipt Actions -->
-    <div class="flex space-x-2 mt-3 pt-3 border-t border-gray-700">
-      <NuxtLink to="/receipt/1/people" class="flex-1 bg-gray-700 text-white py-2 rounded text-sm flex items-center justify-center">
-        <UIcon name="i-heroicons-user-group" :size="16" class="inline mr-1" />
-        <span class="ml-2">People</span>
-      </NuxtLink>
-      <NuxtLink to="/receipt/1/edit" class="flex-1 bg-orange-500 text-white py-2 rounded text-sm flex items-center justify-center">
-        <UIcon name="i-heroicons-pencil" :size="16" class="inline mr-1" />
-        <span class="ml-2">Edit</span>
-      </NuxtLink>
-    </div>
-</div>
-    
     </div>
   </div>
+</template>
+
+<template v-else>
+    Loading...
+  </template>
 </template>
 
 <script setup>
@@ -71,17 +76,21 @@ const id    = route.params.id
 const client = useSupabaseClient()
 
 
-const { data: receiptItems, status } = useAsyncData('receipt_items', async () => {
+const { data: receiptItems, status: itemsStatus } = useAsyncData('receipt_items', async () => {
   const { data, error } = await client.from('receipt_items').select('*').eq('receipt_id', id)
   return data ?? []
 })
 
-// Mock data for the expense with receipt`z items
-const expense = {
-  title: 'Sicily Restaurant',
-  id      : 1,
-  merchant: 'Pizzeria Dom Gino',
-  date    : 'Sep 12, 2023',
-  total   : 94.87,
+const { data: receipt, status: receiptStatus } = useAsyncData('receipt', async () => {
+  const { data, error } = await client.from('receipts').select('*').eq('id', id).single()
+  return data ?? []
+})
+
+function formatDate(date) {
+  return new Date(date).toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  })
 }
 </script> 
