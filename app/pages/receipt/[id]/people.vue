fccef23878f238059a3887fb3be135045c91c477
Saajan Patel <saajan.patel@hoppstudio.com>
2025-06-21 19:18:59 +0100

    feat: people screen styling

--- app/pages/receipt/[id]/people.vue
+++ app/pages/receipt/[id]/people.vue
@@ -7,37 +7,38 @@
         <h1 class="text-xl font-semibold">People</h1>
      <!-- Participants -->
      <div class="mb-6">
-      <div 
-        v-for="participant in expense.participants" 
-        :key="participant.id" 
-        class="bg-gray-800 rounded-lg p-4 mb-2 flex justify-between items-center"
-      >
-        <div class="flex items-center">
-          <div 
-            class="w-10 h-10 rounded-full bg-purple-500 flex items-center justify-center mr-3"
-          >
-            <UIcon name="i-heroicons-user" :size="20" class="text-white" />
-          </div>
-          <div>
-            <h3 class="font-medium">{{ participant.name }}</h3>
-            <span class="text-xs text-gray-400">Sep 21</span>
-          </div>
+        <PeopleCard 
+          v-for="person in people" 
+          :key="person.id"
+          :person="person"
+        />
+        
+        <div class="flex gap-2 mb-4 w-full">
+          <UInput v-model="newPerson" placeholder="Enter a name" class="w-full" />
+          <UButton variant="ghost" class="flex items-center my-6 cursor-pointer" @click="addPerson">
+            <UIcon name="i-heroicons-plus" :size="24" />
+            <span class="ml-2">Add Person</span>
+          </UButton> 
         </div>
-        <span class="font-semibold">
-          ${{ participant.amount.toFixed(2) }}
-            </span>
-        </div>
-        </div>
+  
+     </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const expense = ref({
-  participants: [
+
+const people = ref([
     { id: 1, name: 'John Doe', amount: 100 },
     { id: 2, name: 'Jane Smith', amount: 200 },
-  ],
-});
+    { id: 3, name: 'John Doe', amount: 100 },
+])
+
+const newPerson = ref('')
+
+const addPerson = () => {
+  people.value.push({ id: people.value.length + 1, name: newPerson.value, amount: 0 })
+  newPerson.value = ''
+}
+
 </script>
